c155da048534282b21e61d50bb598bd3498ea6a6
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-03-12 12:31:39 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (25.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,13 +1,39 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        for(int i = 0; i <nums.length; i++){
+//         //O(N) approch is
+//         for(int i = 0; i <nums.length; i++){
             
-            if(nums[i] == target){
-                return i;
+//             if(nums[i] == target){
+//                 return i;
+//             }
+//         }
+//         return -1;
+        
+        //need to do in optimize approch O(log n)
+        //binary search
+        int low = 0;
+        int heigh = nums.length-1;
+        while(low<=heigh){
+            int mid = low + (heigh - low)/2;
+            if(nums[mid] ==  target) return mid;
+            
+            if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target <= nums[mid]){
+                    heigh = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                if(target >= nums[mid] && target <= nums[heigh]){
+                    low = mid +1;
+                }else{
+                    heigh = mid-1;
+                }
             }
         }
         return -1;
+        
         
     }
 }

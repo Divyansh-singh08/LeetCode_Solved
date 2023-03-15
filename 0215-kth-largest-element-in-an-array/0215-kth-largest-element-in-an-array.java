7740263093303f78bbc19af6b5bb0131c2a90daa
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-03-15 07:05:46 +0530

    Time: 46 ms (42.90%), Space: 51.9 MB (39.28%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -8,16 +8,35 @@ class Solution {
         //that will give use more optimize approch
         
         //make PQ of max heap first
+        //by default it follow min heap
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+//         for(int i : nums){
+//             pq.add(i);
+//         }
+//         while(pq.size() - k > 0){
+//             pq.poll();
+//         }
+        
+//         return pq.peek();
+        
+        //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        //to optimize more we use 
+        
+        
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        for(int i : nums){
-            pq.add(i);
+        for(int i =0; i<nums.length;i++){
+            if(pq.size() < k ){
+                pq.add(nums[i]);
+            }else{
+                if(pq.peek()<nums[i]){
+                    pq.poll();
+                    //add new value
+                    pq.add(nums[i]);
+                }
+            }
         }
-        while(pq.size() - k > 0){
-            pq.poll();
-        }
-        
         return pq.peek();
-        
     }
 }

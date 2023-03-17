ac22bfadd45279abd4270ae833244710e548527f
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-03-17 08:17:32 +0530

    Time: 12 ms (61.51%), Space: 56.3 MB (45.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -14,11 +14,23 @@ class Solution {
         //optimal approch
          
         //first sort the array
-        Arrays.sort(nums);
-        for(int i = 0; i < n-1; i++){
-            if(nums[i] == nums[i+1]){
+        // Arrays.sort(nums);
+        // for(int i = 0; i < n-1; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        
+        //optimal approch
+        Set<Integer> uinq = new HashSet<Integer>();
+        for(int i = 0; i < n; i++){
+            //logic
+            if(uinq.contains(nums[i])){
                 return true;
             }
+            uinq.add(nums[i]);
+            
         }
         return false;
         

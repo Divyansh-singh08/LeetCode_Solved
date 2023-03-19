896258f41633d4698d093d601ed205c022b39c1b
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-03-19 12:22:29 +0530

    Added solution - LeetHub

---
+++ Overlapping Intervals - GFG/overlapping-intervals.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] s = br.readLine().trim().split(" ");
+            int[][] Intervals = new int[n][2];
+            int j = 0;
+            for(int i = 0; i < n; i++){
+                Intervals[i][0] = Integer.parseInt(s[j]);
+                j++;
+                Intervals[i][1] = Integer.parseInt(s[j]);
+                j++;
+            }
+            Solution obj = new Solution();
+            int[][] ans = obj.overlappedInterval(Intervals);
+            for(int i = 0; i < ans.length; i++){
+                for(j = 0; j < ans[i].length; j++){
+                    System.out.print(ans[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int[][] overlappedInterval(int[][] Intervals)
+    {
+        // Code here
+         ArrayList<int[]> res = new ArrayList<>();//make arraylist
+         //check the base case
+         
+         if(Intervals.length == 0 ||Intervals == null) return res.toArray(new int[0][]);//empty array return
+         
+         //sort the array
+         Arrays.sort(Intervals,Comparator.comparingInt(a->a[0]));
+         
+         int s = Intervals[0][0];
+         int e = Intervals[0][1];
+         
+         for(int i[]: Intervals){
+             
+             if(i[0]<=e){
+                 e = Math.max(e,i[1]);
+             }else{
+                 res.add(new int[]{s,e});
+                 s=i[0];
+                 e=i[1];
+             }
+         }
+         res.add(new int[]{s,e});
+         return res.toArray(new int[0][]);
+    }
+}

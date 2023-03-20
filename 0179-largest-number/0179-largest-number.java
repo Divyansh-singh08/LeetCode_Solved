c45d575c6cd8ebf3a1cd081581cc4dae5711b2ac
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-03-20 10:02:19 +0530

    Time: 5 ms (97.49%), Space: 42.4 MB (35.03%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -10,16 +10,18 @@ class Solution {
             st[i] = String.valueOf(nums[i]);
         }
         //as we need in decendeing order
+        //this will handle all age case and compair and sort them
         Arrays.sort(st,(a,b)->(b + a).compareTo(a + b));
         
         //we use stringbuler for concatenate all string
-        
+        ////we use stringbuler for concatenate all string into it from the arrayString
         StringBuilder sb = new StringBuilder();
         for(String str : st){
             sb.append(str);
         }
         
         //create another string result
+        ////create another string result for making setbuilder into the String
         String res = sb.toString();
         
         //now just we check

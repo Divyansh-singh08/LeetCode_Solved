12f46a5f3bf06efa34e3a32d6a2ed70f1d3f5c24
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-04-06 08:22:52 +0530

    Time: 4 ms (87.78%), Space: 59.9 MB (62.28%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,27 +2,69 @@ class Solution {
     public int findDuplicate(int[] nums) {
         
         //using Binary Search Tree
-        int low = 0;
-        int high = nums.length-1;
-        while(low <= high){
+//         int low = 0;
+//         int high = nums.length-1;
+//         while(low <= high){
             
-            int mid = (low + high)/2;
-            int count = 0;
-            for(int j : nums){
-                //condition must be check that
-                if(j <= mid){
-                    count++;
-                }
-            }
+//             int mid = (low + high)/2;
+//             int count = 0;
+//             for(int j : nums){
+//                 //condition must be check that
+//                 if(j <= mid){
+//                     count++;
+//                 }
+//             }
             
-            //again check the condition
-            if(count <= mid){
-                low = mid + 1;
-            }else{
-                high = mid - 1;
-            }
+//             //again check the condition
+//             if(count <= mid){
+//                 low = mid + 1;
+//             }else{
+//                 high = mid - 1;
+//             }
+//         }
+//         return low;
+        
+        
+        //TROTOISE approch Using LL cyclic graph
+        int slow = nums[0];//this take one step
+        //fast take 2 step
+        int fast = nums[0];//initial both are sam position
+        do{
+            slow = nums[slow];//this is for single step
+            fast = nums[nums[fast]];//this is for two step
+        }while(slow != fast);
+        
+        //when tha while loop break
+        //then again fast will be at initial index
+        fast = nums[0];
+        //now fast and slow both will take single step
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return low;
+        return fast;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
 }

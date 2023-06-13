f7cd1189291c1bcaf36c7b3fa6167388973ff52b
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-06-13 08:54:49 +0530

    Time: 3 ms (97.82%), Space: 41.8 MB (96.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,63 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-        	return true;
-        }
-        int head = 0, tail = s.length() - 1;
-        char cHead, cTail;
-        while(head <= tail) {
-        	cHead = s.charAt(head);
-        	cTail = s.charAt(tail);
-        	if (!Character.isLetterOrDigit(cHead)) {
-        		head++;
-        	} else if(!Character.isLetterOrDigit(cTail)) {
-        		tail--;
-        	} else {
-        		if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
-        			return false;
-        		}
-        		head++;
-        		tail--;
-        	}
+//         if (s.isEmpty()) {
+//         	return true;
+//         }
+//         int head = 0, tail = s.length() - 1;
+//         char cHead, cTail;
+//         while(head <= tail) {
+//         	cHead = s.charAt(head);
+//         	cTail = s.charAt(tail);
+//         	if (!Character.isLetterOrDigit(cHead)) {
+//         		head++;
+//         	} else if(!Character.isLetterOrDigit(cTail)) {
+//         		tail--;
+//         	} else {
+//         		if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+//         			return false;
+//         		}
+//         		head++;
+//         		tail--;
+//         	}
+//         }
+        
+//         return true;
+        
+        
+        //first check the base case
+        if(s.isEmpty()){
+            return true;
         }
         
-        return true;
+//         you need to use Two pointer Approch and compair it
+        int head = 0;
+        int tail = s.length() - 1;
+        char cHead;
+        char cTail;
+        while(head <= tail){
+            //now point it to the original
+            cHead = s.charAt(head);//point to first value
+            cTail = s.charAt(tail);//point to the last value
+            if(!Character.isLetterOrDigit(cHead)){
+                //false mean that it's character or letter only
+                //true mean that not char nor number
+                head++;
+            }
+            else if(!Character.isLetterOrDigit(cTail)){
+                tail--;
+            }else{
+                //now also need to maintane the locase too
+                if(Character.toLowerCase(cHead) != Character.toLowerCase(cTail)){
+                    //if there are not matching return false
+                    return false;
+                }
+                head++;
+                tail--;
+            }
+        }
+    return true;
+        
+        
+        
     }
 }

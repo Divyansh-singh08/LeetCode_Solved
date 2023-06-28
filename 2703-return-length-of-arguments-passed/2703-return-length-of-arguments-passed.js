47650ffe9bba4cf0230b41e72bc0736c004dd01d
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-06-28 14:14:19 +0530

    Time: 47 ms (96.56%), Space: 41.8 MB (70.63%) - LeetHub

---
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -0,0 +1,14 @@
+/**
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    var count=0;
+ for(let  i = 0; i < args.length; i++){
+     count++;
+ }
+    return count;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */

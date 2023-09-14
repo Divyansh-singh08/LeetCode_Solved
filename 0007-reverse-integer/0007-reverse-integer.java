2b7a8c56dfd6f08430b791e377937197d27da670
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-09-14 12:15:28 +0530

    Time: 1 ms (98.01%), Space: 39.5 MB (78.25%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,13 +1,17 @@
 class Solution {
     public int reverse(int x) {
-        long count = 0;
+        int count = 0;
         while(x != 0){
-            long rem = x%10;
-            // count += rem;
-            count = count*10 + rem;
+            int rem = x%10;
+            int newCount = count*10 + rem;
+            if((newCount - rem)/10 != count){
+                return 0;
+            }
+            count = newCount;
             x = x/10;
         }
-        return (int)count == count ? (int)count : 0;
+        // return (int)count == count ? (int)count : 0;
+        return count;
 
     }
 }

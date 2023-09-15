34ae974bef42cea44492f30ed894bc990f4bcad2
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-09-16 00:13:42 +0530

    Time: 1 ms (90.49%), Space: 39.3 MB (87.82%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,16 +1,26 @@
 class Solution {
     public int addDigits(int num) {
-        
-        while(num >= 10){
+        ///iterative approch
+//         while(num >= 10){
             
-            int sum = 0;
-            while(num > 0){
-                int rem = num%10;
-                sum = sum + rem;
-                num = num/10;
-            }
-            num = sum;
+//             int sum = 0;
+//             while(num > 0){
+//                 int rem = num%10;
+//                 sum = sum + rem;
+//                 num = num/10;
+//             }
+//             num = sum;
+//         }
+//         return num;
+        
+        // ------> //optimal Approch
+        if(num == 0){
+            return num;
+        }else if(num%9 == 0){
+            return 9;
+        }else{
+            return (num%9);
         }
-        return num;
+        
     }
 }

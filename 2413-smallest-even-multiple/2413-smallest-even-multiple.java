e138b39a9989fc867b14fe68e945c3d040d874ee
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-09-16 12:35:24 +0530

    Time: 0 ms (100.00%), Space: 39 MB (74.92%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -1,24 +1,31 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        int m =2;
-        int val = n;
-        int gcd = 1;
-        while( n > 0 && m > 0){
-            if(n > m){
-                n = n % m;
-            }else{
-                m = m % n;
-            }
-        }
+        //  LCM find Using gcd - > T.C O(log min(n.m))
         
-        if(n == 0){
-            gcd = m;
-        }else{
-            gcd = n;
-        }
+//         int m =2;
+//         int val = n;
+//         int gcd = 1;
+//         while( n > 0 && m > 0){
+//             if(n > m){
+//                 n = n % m;
+//             }else{
+//                 m = m % n;
+//             }
+//         }
         
-        int lcm = 2*val / gcd;
-        return lcm;
+//         if(n == 0){
+//             gcd = m;
+//         }else{
+//             gcd = n;
+//         }
+        
+//         int lcm = 2*val / gcd;
+//         return lcm;
+        
+        //    o(1) //
+        
+        return n << (n & 1);
+        
         
     }
 }

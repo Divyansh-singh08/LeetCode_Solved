802421ab34dc0628657fae23e87f00c151ce87ca
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-09-19 15:46:05 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (98.01%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -12,30 +12,30 @@ class Solution {
         //     nums[i++] = n;
         // return i;
         
-//         int response = 0;
-//         int n = nums.length;
-//         for(int i = 0; i<nums.length; i++){
-            
-//             if(nums[i] > nums[(i+1)%n]){
-//                 response += 1;
-//             }
-//         }
-        
-//         return response > 1 ? false : true;
-        
+        int response = 0;
         int n = nums.length;
-        int cnt = 0;
-        for(int i = 1; i<n; i++){
-            if(nums[i-1] > nums[i]){
-                cnt++;
+        for(int i = 0; i<nums.length; i++){
+            
+            if(nums[i] > nums[(i+1)%n]){
+                response += 1;
             }
         }
         
-        if(nums[n-1] > nums[0]){
-            cnt++;
-        }
+        return response > 1 ? false : true;
         
-        return cnt<=1;
+//         int n = nums.length;
+//         int cnt = 0;
+//         for(int i = 1; i<n; i++){
+//             if(nums[i-1] > nums[i]){
+//                 cnt++;
+//             }
+//         }
+        
+//         if(nums[n-1] > nums[0]){
+//             cnt++;
+//         }
+        
+//         return cnt<=1;
             
       
     }

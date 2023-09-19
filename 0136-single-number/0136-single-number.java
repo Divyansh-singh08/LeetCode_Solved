addfd481acde5fb67e3dc4f468c204aac54447c3
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-09-19 22:35:17 +0530

    Time: 1 ms (99.96%), Space: 44.2 MB (75.15%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,32 +1,35 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        
-///////////////// brute force approch O(N^2) /////////////////////////
-        
-//         int n = nums.length;
-//         int ans = 0;
-//         for(int i = 0; i<n; i++){
-//             int ans1 = nums[i];
-//             int ctn = 0;
-//             for(int j = 0; j<n; j++){
-                
-//                 if(nums[j] == ans1){
-//                     ctn++;
-//                 }
-//             }
-//             if(ctn == 1){
-//                 ans = ans1;
-//             }
-//         }
-//         return ans;
-        
-        
-        ///////////// optimal -> way to do it is XOR
-        //doing XOR to any number if it present twice then it will became zero
-        int xorAns = 0;
+        int xor = 0;
         for(int i = 0; i<nums.length; i++){
-            xorAns = xorAns ^ nums[i];
+            
+            xor = xor^nums[i];
         }
-        return xorAns;
+        return xor;
     }
 }
+
+/*
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for(int i = 0; i<n; i++){
+            int ans1 = nums[i];
+            int ctn = 0;
+            for(int j = 0; j<n; j++){
+                
+                if(nums[j] == ans1){
+                    ctn++;
+                }
+            }
+            if(ctn == 1){
+                ans = ans1;
+            }
+        }
+        return ans;
+    }
+}
+
+*/

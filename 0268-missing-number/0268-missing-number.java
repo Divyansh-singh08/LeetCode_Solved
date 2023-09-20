98107fd19a9106d5f655e81ab90a35f43fe854b1
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-09-20 23:53:44 +0530

    Time: 262 ms (5.01%), Space: 44 MB (68.50%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,32 @@
 class Solution {
     public int missingNumber(int[] nums) {
         
-        int sum = 0; 
-        int n = nums.length;
-        int mis = (n*(n+1))/2;
-        for(int i = 0; i<n; i++){
-            sum = sum + nums[i];
+        //brute force approch
+        int ans = 0;
+        for(int i = 1; i<=nums.length; i++){
+            
+            int flag = 0;
+            for(int j = 0; j<nums.length; j++){
+                if(nums[j] == i){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 0){
+                ans = i;
+            }
         }
+        return  ans;
         
-        return (mis-sum);
+        
+        //this is the best Approch not Optimal
+//         int sum = 0; 
+//         int n = nums.length;
+//         int mis = (n*(n+1))/2;
+//         for(int i = 0; i<n; i++){
+//             sum = sum + nums[i];
+//         }
+        
+//         return (mis-sum);
     }
 }

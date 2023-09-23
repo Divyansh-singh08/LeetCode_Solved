29b7e3a0d3234c1791f7c5eee19a7c2ad9ce632d
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-09-23 13:15:23 +0530

    Time: 442 ms (23.36%), Space: 50.8 MB (48.19%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         
-        //3Pointer technique we used here in O(N^2)
-        
-        Set<List<Integer>> res = new HashSet<>();//this is for storing the unique list
-        
+        Set<List<Integer>> res = new HashSet<>();
+        //if there is empty array
         if(nums.length == 0){
             return new ArrayList<>(res);
         }
-        //do first storting for 3 tecnique
+        //sort the array
         Arrays.sort(nums);
         
-        //for makeing one fixed Pointer we use 2 loop
-        for(int i = 0; i < nums.length - 2 ;i++){
+        for(int i = 0; i<nums.length-2; i++){
+            
+            // int sum = nums[i];
             int j = i+1;
             int k = nums.length-1;
-            while(j < k ){
-                int sum = nums[j]+nums[k];
-                if(sum == -nums[i]){
+            
+            while(j<k){
+                
+                int sum = nums[j] + nums[k];
+                if(sum > -nums[i]){
+                    k--;
+                }
+                else if(sum < -nums[i]){
+                    j++;
+                }
+                else {
+                    //if equal mean one case match
                     res.add(Arrays.asList(nums[i],nums[j],nums[k]));
                     j++;
                     k--;
-                }else if(sum > -nums[i]){
-                    k--;//sum more then decrease
-                }else if(sum < -nums[i]){
-                    j++;//sum less then do increase
                 }
-            } 
+            }
         }
         return new ArrayList<>(res);
         

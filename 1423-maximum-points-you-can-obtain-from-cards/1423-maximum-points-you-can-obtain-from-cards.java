68eb5d1c903e7fbe0d8ee9fb5e01de3a658f4f78
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-10-02 00:01:03 +0530

    Time: 2 ms (96.91%), Space: 55.4 MB (60.86%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,23 +1,58 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
+
         int n = cardPoints.length;
+        
         int sum = 0;
-        int j = n-1;
-        int i;
-        for(i = 0; i<k; i++){
+        
+        //add the total sum of array
+        for(int i = 0; i < n; i++){
             sum+=cardPoints[i];
         }
-        
-        i--;
-        int ans = sum;
-        
-        for(j = n-1; j>n-1-k; j--){
-            sum = sum - cardPoints[i] + cardPoints[j];
-            i--;
-            ans = Math.max(ans,sum);
+        if(n == k) return sum;
+        int maxValue = 0;
+        //now manging the  window 
+        int slidingWindow = 0;
+        for(int i = 0; i<n-k-1; i++){
+            slidingWindow = slidingWindow + cardPoints[i];
         }
         
-        return ans;
+        for(int i = n-k-1; i<n; i++){
+            slidingWindow += cardPoints[i];
+            maxValue = Math.max(maxValue, (sum - slidingWindow));
+            slidingWindow -= cardPoints[i - (n-k-1)];
+            
+        }
+        
+        return maxValue;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //         int n = cardPoints.length;
+//         int sum = 0;
+//         int j = n-1;
+//         int i;
+//         for(i = 0; i<k; i++){
+//             sum+=cardPoints[i];
+//         }
+        
+//         i--;
+//         int ans = sum;
+        
+//         for(j = n-1; j>n-1-k; j--){
+//             sum = sum - cardPoints[i] + cardPoints[j];
+//             i--;
+//             ans = Math.max(ans,sum);
+//         }
+        
+//         return ans;
         
     }
 }

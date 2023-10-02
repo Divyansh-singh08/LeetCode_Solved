38dc94a4bb3e8e7d103772820527bbb5fd37cf25
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-10-02 14:05:26 +0530

    Time: 3 ms (9.94%), Space: 60.6 MB (83.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,23 +1,39 @@
 class Solution {
     public int maxProfit(int[] prices) {
         
-        int maxProfit = 0;
-        int minValue = Integer.MAX_VALUE;
-        //ITERATE To the array
-        for(int i = 0; i<prices.length; i++){
-            //Condition for checking the minValue 
-            //so that we can take max profit
-            if(prices[i] < minValue){
-                //if true prices[i] will now min value
-                minValue = prices[i];
-            }
-            //now check Max profit
-            // iterate to the array and check the max profit 
-            maxProfit = Math.max(maxProfit,(prices[i] - minValue));
-            
+        
+        //Let do it using Kadane Alogrithm
+        
+        int maxAlltime = 0;
+        int maxValue = 0;
+        for(int i = 1; i<prices.length; i++){
+            //check is  price is max or not
+            //we only take positive value so that why we use 0 any greater then 0
+            //
+            maxValue = Math.max(0,maxValue += prices[i] - prices[i-1]);
+            maxAlltime = Math.max(maxAlltime,maxValue);
         }
         
-        return maxProfit;
+        return maxAlltime;
+        
+        
+//         int maxProfit = 0;
+//         int minValue = Integer.MAX_VALUE;
+//         //ITERATE To the array
+//         for(int i = 0; i<prices.length; i++){
+//             //Condition for checking the minValue 
+//             //so that we can take max profit
+//             if(prices[i] < minValue){
+//                 //if true prices[i] will now min value
+//                 minValue = prices[i];
+//             }
+//             //now check Max profit
+//             // iterate to the array and check the max profit 
+//             maxProfit = Math.max(maxProfit,(prices[i] - minValue));
+            
+//         }
+        
+//         return maxProfit;
         
         
         

8654e8615816261ab279a07c6aecc833804ce798
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-10-02 15:28:21 +0530

    Time: 1 ms (99.85%), Space: 47.8 MB (70.14%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,30 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
+        
+        //solve using Moores voting alogrithem
+        
+        int count = 0;
+        int current = 0;
+        
+        for(int i = 0; i<nums.length; i++){
+            
+            if(count == 0){
+                current = nums[i];
+            }
+            
+            if(nums[i] == current){
+                count++;
+            }else{
+                count--;
+            }
+            
+        }
+        
+        return current;
+        
+        
     }
 }

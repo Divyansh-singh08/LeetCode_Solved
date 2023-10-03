f0d276c0f5e867b052d1c3a4cc7422f9830a21fd
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-10-03 08:22:08 +0530

    Time: 1 ms (24.88%), Space: 41 MB (44.15%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,28 +1,34 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int low = 0;
-        int mid = 0;
-        int heigh = n - 1;
+        
+        //1st Method
+        Arrays.sort(nums);
+        
+        
+        
+//         int n = nums.length;
+//         int low = 0;
+//         int mid = 0;
+//         int heigh = n - 1;
 
-        // for iteration use while loop
-        while(mid <= heigh){
-            //condition check
-            if(nums[mid] == 0){
-                //swap
-                int temp = nums[mid];
-                nums[mid] = nums[low];
-                nums[low] = temp;
-                low++;
-                mid++;
-            }else if(nums[mid] == 1){
-                mid++;
-            }else{
-                int temp = nums[mid];
-                nums[mid] = nums[heigh];
-                nums[heigh] = temp;
-                heigh--;
-            }
-        }
+//         // for iteration use while loop
+//         while(mid <= heigh){
+//             //condition check
+//             if(nums[mid] == 0){
+//                 //swap
+//                 int temp = nums[mid];
+//                 nums[mid] = nums[low];
+//                 nums[low] = temp;
+//                 low++;
+//                 mid++;
+//             }else if(nums[mid] == 1){
+//                 mid++;
+//             }else{
+//                 int temp = nums[mid];
+//                 nums[mid] = nums[heigh];
+//                 nums[heigh] = temp;
+//                 heigh--;
+//             }
+//         }
     }
 }

3c1bf3cf97d3c0b8b521bc1bd1afb5b39f3eaa44
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-10-04 21:31:59 +0530

    Time: 0 ms (100.00%), Space: 43 MB (48.01%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,25 +1,43 @@
 class Solution {
       public boolean search(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (nums[mid] == target) return true;
-
-            if (nums[low] == nums[mid]) {
-                low++;
-                continue;
-            }
-
-            if (nums[low] <= nums[mid]) {
-                if (nums[low] <= target && target <= nums[mid]) high = mid - 1;
-                else low = mid + 1;
-            } else {
-                if (nums[mid] <= target && target <= nums[high]) low = mid + 1;
-                else high = mid - 1;
-            }
-        }
-        return false;
+          
+          int l = 0;
+          int h = nums.length-1;
+          
+          while(l<=h){
+              
+              int mid = (l + h)/2;
+              //if equal the value required
+              if(nums[mid] == target) return true;
+              
+              //Some IMP Edge Case
+              if(nums[l] == nums[mid] && nums[mid] == nums[h]){
+                  l++;
+                  h--;
+                  continue;
+              }
+             // low  is less then mid
+              if( nums[l] <= nums[mid]){
+                  //check to the left side
+                  if(nums[l] <= target && target <= nums[mid]){
+                      h = mid - 1;
+                  }else{
+                      l = mid + 1;
+                  }
+        
+              }else{
+                  //check to the right side
+                  if(nums[mid] <= target && target <= nums[h]){
+                      l = mid + 1;
+                  }else{
+                      h = mid - 1;
+                  }
+                  
+              }
+              
+              
+          }
+          return false;
     }
     
 }

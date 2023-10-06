aea27c2c59d98a380ed5a2a38706639bcc042e37
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-10-06 23:11:07 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.java
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.java
@@ -30,29 +30,89 @@ class GFG {
 // User function Template for Java
 
 class Solve {
-    int[] findTwoElement(int arr[], int n) {
+    int[] findTwoElement(int arr[], int N) {
         // code here
+        //maing Optimal Approch O(N) time
+        //here we basically using Sum technique
+        long n = (long)N;
         
-        //Make Better Approch to solve this...
-        int mis = -1,dup = -1;
-        int[] hash = new int[n+1];
+        int dup = -1,mis = -1;
         
+        //sum of all element till the range 1 to N
+        long Nsum = (n * (n+1))/2; //21
+        //SquareSum of all element till the range 1 to N
+        long NsumSquare = (n * (n+1) * (2*n+1))/6;   
+        
+        long sum = 0;
+        long SquareSum = 0;
+        
+        //sum of all the array elements 0 to N-1
+        //square Sum of all array element 0 to N-1
         for(int i = 0; i<n; i++){
-            hash[arr[i]]++; 
+            sum += arr[i]; //17
+            SquareSum += (long)arr[i] * (long)arr[i];
         }
-        int i=1;
-        while(i <n+1){
-            //there is three condition
-            if(hash[i] == 2){
-               dup = i; 
-            }else if(hash[i] == 0){
-                mis = i;
-            }
-            
-            i++;
-        }
+        
+             
+        //this is the first Equation
+        long ans1 = sum - Nsum; //-4
+        //this is the second Equation
+        long ans2 = SquareSum - NsumSquare; //-24
+        
+        // int mainResult = ans2 /ans1;
+        //actually be are upadting ans2 -24 to ----> 6
+        //ans2 = 6 
+        ans2 /= ans1;
+        
+        //as we know ans2 = 1 
+        //-4 + 6 = 2/2 = ** 1 **
+        long ans3Dup =(ans1 + ans2)/2;
+        
+        // 6 - 1 = 5 -> missing element
+        long ans3mis = ans2 - ans3Dup;
+        
+        //you get the duplicate element
+        dup = (int)ans3Dup;
+        //missing element
+        mis = (int)ans3mis;
         
         return new int[]{dup,mis};
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //Make Better Approch to solve this...
+        // int mis = -1,dup = -1;
+        // int[] hash = new int[n+1];
+        
+        // for(int i = 0; i<n; i++){
+        //     hash[arr[i]]++; 
+        // }
+        
+        // int i=1;
+        // while(i <= n){
+        //     //there is three condition
+        //     if(hash[i] == 2){
+        //       dup = i; 
+        //     }else if(hash[i] == 0){
+        //         mis = i;
+        //     }
+        //     //edge case must follow
+        //     if(dup != -1 && mis != -1){
+        //         break;
+        //     }
+        //     i++;
+        // }
+        
+        // return new int[]{dup,mis};
         
         
         

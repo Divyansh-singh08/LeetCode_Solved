3aa425f227f2312432c831ad6c6c1b2a4d65baa5
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-10-11 11:55:21 +0530

    Time: 10 ms (76.75%), Space: 44.9 MB (78.91%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,54 @@
+class Solution {
+    
+    static int checkIt(int[] arr,int capacity){
+        int days = 1;
+        int load = 0;
+        
+        for(int i = 0; i<arr.length; i++){
+            
+            if(load + arr[i] > capacity){
+                days = days + 1;
+                load = arr[i];
+            }else{
+                load = load + arr[i];
+            }
+        }
+        
+        return days;
+    }
+    
+    
+    public int shipWithinDays(int[] weights, int days) {
+        
+//         int n = weights.length;
+//         int max = Integer.MIN_VALUE;
+//         for(int i = 0 ; i<n; i++){
+            
+//             if(max < weights[i]){
+//                 max = weights[i];
+//             }
+//         }
+        
+        int l = Integer.MIN_VALUE, h = 0;
+        for (int i = 0; i < weights.length; i++) {
+            h += weights[i];
+            l = Math.max(l, weights[i]);
+        }
+
+        // int l = max; 
+        // int h = (n*(n+1))/2;
+        int res = 1;
+        while(l <= h){
+            
+            int mid = (l+h)/2;
+            
+            int noofDays = checkIt(weights,mid);
+            if(noofDays <= days){
+                h = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}

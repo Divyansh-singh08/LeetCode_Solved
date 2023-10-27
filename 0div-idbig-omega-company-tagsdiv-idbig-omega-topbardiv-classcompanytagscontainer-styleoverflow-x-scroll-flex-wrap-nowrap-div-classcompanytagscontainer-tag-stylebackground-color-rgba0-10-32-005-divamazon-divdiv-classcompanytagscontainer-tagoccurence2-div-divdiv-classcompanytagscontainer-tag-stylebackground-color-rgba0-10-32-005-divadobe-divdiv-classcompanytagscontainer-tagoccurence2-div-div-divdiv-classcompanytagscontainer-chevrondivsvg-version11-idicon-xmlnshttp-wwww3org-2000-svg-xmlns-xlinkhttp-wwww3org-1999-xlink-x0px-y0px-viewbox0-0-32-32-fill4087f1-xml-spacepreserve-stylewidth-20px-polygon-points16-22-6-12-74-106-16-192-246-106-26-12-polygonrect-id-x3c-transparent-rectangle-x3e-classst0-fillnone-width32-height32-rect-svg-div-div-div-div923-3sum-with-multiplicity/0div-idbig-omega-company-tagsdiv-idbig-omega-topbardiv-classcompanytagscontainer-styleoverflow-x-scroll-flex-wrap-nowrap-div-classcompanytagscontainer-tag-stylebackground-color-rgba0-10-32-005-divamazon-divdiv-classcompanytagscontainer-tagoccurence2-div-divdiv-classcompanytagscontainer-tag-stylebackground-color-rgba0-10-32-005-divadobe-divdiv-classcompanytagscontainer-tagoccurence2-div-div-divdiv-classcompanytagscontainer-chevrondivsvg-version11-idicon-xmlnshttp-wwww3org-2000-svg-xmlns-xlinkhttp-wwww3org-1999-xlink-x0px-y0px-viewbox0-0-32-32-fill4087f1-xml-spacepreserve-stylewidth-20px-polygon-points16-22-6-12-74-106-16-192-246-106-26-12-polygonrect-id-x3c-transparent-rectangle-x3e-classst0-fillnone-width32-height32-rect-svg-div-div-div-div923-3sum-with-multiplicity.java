6ae4ee421a044b16a20b00a796cf9d46805728cc
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-10-27 09:19:26 +0530

    Time: 2 ms (100.00%), Space: 43.6 MB (34.55%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div923-3sum-with-multiplicity/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div923-3sum-with-multiplicity.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div923-3sum-with-multiplicity/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div923-3sum-with-multiplicity.java
@@ -1,7 +1,81 @@
 class Solution {
     public int threeSumMulti(int[] arr, int target) {
         
-        int n = arr.length;
+        
+        
+        
+        int mod = 1_000_000_007;
+        long result = 0; 
+        long[] c = new long[101];
+        for(int i : arr) c[i]++;
+        
+        for(int i = 0; i <= 100; i++) {
+            for(int j = i; j <= 100; j++) {
+                int k = target-i-j;
+                if(k<0 || k >100) continue;
+                if(i == j && j==k) {
+                    result+= (c[i] * (c[i]-1) * (c[i]-2) /6);
+                }
+                else if(i == j && j != k) {
+                    result += ((c[i] * (c[i]-1) /2 ) * c[k]);
+                }
+                else if(i < j && j < k) {
+                    result += (c[i] * c[j] * c[k]);
+                }
+            }
+        }
+        return (int)(result%mod);
+        
+//         // int n = arr.length;
+//         //OPTIMAL SOLUTION
+//         int mod = 1_000_000_007;
+//         long res = 0;
+//         long []cnt = new long[101];
+        
+//         for(int i : arr){
+//             cnt[i]++;
+//         }
+        
+//         for(int i = 0;i<=100; i++){
+//             for(int j = i; j<=100; j++){
+//                 int k = target - i - j;
+                
+//                 if(k < 0 || k > 100) continue;
+                
+//                 if(i == j && j == k){
+//                     res = res + (cnt[i] * (cnt[i]-1) * (cnt[i]-2) / 6);
+                    
+//                 }else if( i == j && i != j){
+//                     res = res + (((cnt[i] * (cnt[i]-1))/2) * cnt[k]);   
+//                 }else if(i < j && j < k){
+//                     res = res + (cnt[i] * cnt[j] * cnt[k]);
+//                 }
+//             }
+//         }
+        
+//         return (int)(res%mod);
+    }
+}
+        
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 //         // int sum = 0;
 //         int cnt = 0;
 //         for(int i = 0; i<n-2; i++){
@@ -25,21 +99,28 @@ class Solution {
 //         return cnt;
         
         
-        int mod = 1_000_000_007;
-        long result = 0;
         
-        for(int i = 0; i<n; i++){
-            int count[] = new int[101];
-            for(int j = i+1; j<n; j++){
-                int k = target - arr[i] - arr[j];
+        
+        
+        
+        
+//         // T.C - >O(n^2) S.P -O(1)
+        
+//         int mod = 1_000_000_007;
+//         long result = 0;
+        
+//         for(int i = 0; i<n; i++){
+//             int count[] = new int[101];
+//             for(int j = i+1; j<n; j++){
+//                 int k = target - arr[i] - arr[j];
                 
-                if(k>=0 && k<=100 && count[k]>0){
-                    result = result + count[k];
-                    result = result%mod;
-                }
-                count[arr[j]]++;
-            }
-        }
-        return (int)result;
-    }
-}
+//                 if(k>=0 && k<=100 && count[k]>0){
+//                     result = result + count[k];
+//                     result = result%mod;
+//                 }
+//                 count[arr[j]]++;
+//             }
+//         }
+//         return (int)result;
+    // }
+// }

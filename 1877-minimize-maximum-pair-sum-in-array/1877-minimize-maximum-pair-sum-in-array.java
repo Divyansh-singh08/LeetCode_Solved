32f06bfa0039a3afecb9569491655cf47bb05eb0
Divyansh Singh <70822782+Divyansh-singh08@users.noreply.github.com>
2023-11-17 20:22:24 +0530

    Time: 7 ms (99.51%), Space: 59.4 MB (5.10%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -1,22 +1,72 @@
 class Solution {
     public int minPairSum(int[] nums) {
         
-        int n = nums.length;
-        if(n%2 != 0){
-            return -1;
-        }
-        int maxAns = Integer.MIN_VALUE;
-        int left = 0;
-        int right = n-1;
-        Arrays.sort(nums);
-        while(left < right){
+        //this is one ways T.C ->O(n log n)
+//         int n = nums.length;
+//         if(n%2 != 0){
+//             return -1;
+//         }
+//         int maxAns = Integer.MIN_VALUE;
+//         int left = 0;
+//         int right = n-1;
+//         Arrays.sort(nums);
+//         while(left < right){
             
-            maxAns = Math.max(maxAns,nums[left]+nums[right]);
-            left++;
-            right--;
+//             maxAns = Math.max(maxAns,nums[left]+nums[right]);
+//             left++;
+//             right--;
+//         }
+        
+//         return maxAns;
+        
+        
+        
+        //SPACE COMPLIXITY IS MORE O(Max(n))
+        //TIME IS O(N) TIME IT TAKE
+        
+        
+        int n = nums.length;
+        int max = 0;
+        for(int i = 0; i<n; i++){
+            if(max < nums[i]){
+                max = nums[i];
+            }
         }
         
-        return maxAns;
         
+        
+        int[] arr = new int[max+1];
+        //using the hashing technique
+        for(int i = 0; i<n; i++){
+            arr[nums[i]]+=1;
+        }
+        
+        int temp = 0;
+        int i = 0;
+        int j = max;
+        while(i<=max){
+            
+            if(arr[i] == 0){
+                i++;
+                continue;
+            }
+            
+            if(arr[j] == 0){
+                j--;
+                continue;
+            }
+            
+            
+            if(arr[i] !=0 && arr[j] != 0){
+                
+                if(i+j > temp){
+                    temp = i+j;
+                }
+                arr[i]--;
+                arr[j]--;
+            }
+        }
+        
+        return temp;
     }
 }
